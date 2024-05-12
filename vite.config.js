03bdcf97ecfd89641b97e148004e7092477c0b9c
Sai Krishna Kanteti <saikrishna.kanteti@gmail.com>
2024-05-12 10:34:15 +0530

    updated backend api for config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react-swc'
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://full-stack-app-blond.vercel.app/',
+        changeOrigin: true,
+      },
+    },
+  },
   plugins: [react()],
-})
+});
